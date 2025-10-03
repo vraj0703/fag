e4fd051d3a80b5f3fbd9776c2b19aa7585c9a88a
vraj0703 <vraj0703@gmail.com>
2025-10-03 16:50:05 +0530

    checkpoint

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-import yaml
-
-# Load the YAML config
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-CONFIG_FILE = os.path.join(BASE_DIR, "config.yaml")
-with open(CONFIG_FILE, "r") as f:
-    config = yaml.safe_load(f)
